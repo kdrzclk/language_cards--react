aa441c80fbffc57697e09557fe3c538745983704
kdrzclk <kadir.ozcelik16@gmail.com>
2022-03-26 18:09:58 +0300

    updated

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,14 +1,17 @@
 import { languages } from "../../helpers/data.js";
 import Item from "../item/Item.js";
+import "./Card.css";
 
 const Card = () => {
   return (
     <div className="card-area-container">
       {/* <div className="bars"></div> */}
       <h1 className="language-title">Languages</h1>
-      {languages.map((item, index) => {
-        return <Item card={item} key={index} />;
-      })}
+      <div className="cards-container">
+        {languages.map((item, index) => {
+          return <Item card={item} key={index} />;
+        })}
+      </div>
     </div>
   );
 };
